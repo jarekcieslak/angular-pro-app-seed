daf1d07722b1b8fd681938395aa7643e4b35f611
Jaroslaw Cieslak <jarekcieslak@gmail.com>
2017-10-06 12:11:28 +0200

    Fixed formatting

--- src/app/containers/app/app.component.ts
+++ src/app/containers/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  styleUrls: ['app.component.scss'],
-  template: `
-    <div>
-      Hello Ultimate Angular!
-    </div>
-  `
+    selector: 'app-root',
+    styleUrls: ['app.component.scss'],
+    template: `
+        <div>
+            Hello Ultimate Angular!
+        </div>
+    `
 })
 export class AppComponent {
-  constructor() {}
+    constructor() {
+    }
 }
